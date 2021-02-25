0760883b24dda8f1ccd1fb622e7604c40f2c9dac
huynhphu283 <62682189+huynhphu283@users.noreply.github.com>
2021-02-25 17:08:59 +0700

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -15,10 +15,48 @@ sudo dscl . -passwd /Users/vncuser $1
 sudo createhomedir -c -u vncuser > /dev/null
 
 #Enable VNC
-sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate
-sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -allowAccessFor -allUsers -privs -all
-sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -clientopts -setdirlogins -dirlogins yes
-sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -restart -agent
+KICKSTART="/System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart";
+createARDAdminGroup() {
+    dscl . -read /Groups/ard_admin  > /dev/null 2>&1
+    if [ $? -ne 0 ]; then
+        echo "Creating group ard_admin";
+        dseditgroup -o create -r "ARD Admins" ard_admin;
+    else
+        echo "Group ard_admin already exists";
+    fi
+}
+addAdminGroupToARD_admin() {
+        echo "Adding $ADMINGROUP to ard_admin";
+        SAVEIFS=$IFS
+        IFS=$(echo -en "\n\b")
+        ADMINGROUPS=$(echo "$ADMINGROUP" | tr "," "\n");
+        for AGROUP in $ADMINGROUPS; do
+            echo "GROUP; $AGROUP";
+            dseditgroup -o edit -a "$AGROUP" -t group ard_admin;
+        done
+        IFS=$SAVEIFS
+}
+if [ "$ADMINUSER" == "" ]; then
+    echo "No admin user specified";
+    ADMINUSER="$DEFAULTADMIN";
+else
+    ADMINUSER="$ADMINUSER,$DEFAULTADMIN";
+fi
+echo "Clearing ARD Settings"
+$KICKSTART -uninstall -settings
+#ENABLE ARD FOR DEFAULT ADMINS
+$KICKSTART -configure -allowAccessFor -specifiedUsers
+$KICKSTART -configure -users $ADMINUSER -access -on -privs  -all
+if [ "$ADMINGROUP" == "" ]; then
+        echo "No admin group specified skipping directory authentication config";
+        $KICKSTART -configure -clientopts -setreqperm -reqperm yes
+else
+    createARDAdminGroup;
+    addAdminGroupToARD_admin;
+    $KICKSTART -configure -users ard_admin -access -on -privs -all
+    $KICKSTART -configure -clientopts -setreqperm -reqperm yes -setdirlogins -dirlogins yes
+fi
+$KICKSTART -activate -restart -agent
 
 
 #VNC password - http://hints.macworld.com/article.php?story=20071103011608872
